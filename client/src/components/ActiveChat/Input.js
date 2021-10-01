7339740203b72a0530942e86d30dd0cbfcde2d35
Hai Long Do <long2qw@gmail.com>
2021-10-01 18:28:08 -0400

    Cannot submit if input is empty

--- client/src/components/ActiveChat/Input.js
+++ client/src/components/ActiveChat/Input.js
@@ -7,14 +7,14 @@ import { postMessage } from "../../store/utils/thunkCreators";
 const useStyles = makeStyles(() => ({
   root: {
     justifySelf: "flex-end",
-    marginTop: 15
+    marginTop: 15,
   },
   input: {
     height: 70,
     backgroundColor: "#F4F6FA",
     borderRadius: 8,
-    marginBottom: 20
-  }
+    marginBottom: 20,
+  },
 }));
 
 const Input = (props) => {
@@ -23,21 +23,22 @@ const Input = (props) => {
   const { postMessage, otherUser, conversationId, user } = props;
   const handleChange = (event) => {
     setText(event.target.value);
-  
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     // add sender user info if posting to a brand new convo, so that the other user will have access to username, profile pic, etc.
-    const reqBody = {
-      text: event.target.text.value,
-      recipientId: otherUser.id,
-      conversationId,
-      sender: conversationId ? null : user
-    };
-   
-    await postMessage(reqBody);
-    setText("");
+    if (text !== "") {
+      const reqBody = {
+        text: event.target.text.value,
+        recipientId: otherUser.id,
+        conversationId,
+        sender: conversationId ? null : user,
+      };
+
+      await postMessage(reqBody);
+      setText("");
+    }
   };
 
   return (
